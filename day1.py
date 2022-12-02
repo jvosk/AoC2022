7c5b5752f674f82f11864481232408a4e0a5bfd1
Josh Voskamp <joshuavoskamp@gmail.com>
2022-12-01 22:24:09 -0500

    fix sorting logic bug (typed program from memory initially :/ )

--- day1.py
+++ day1.py
@@ -1,3 +1,3 @@
 with open("input1.txt") as f:
-    packages = sorted([[int(i) for i in package.split()] for package in f.read().split("\n\n")])
+    packages = sorted([[int(i) for i in package.split()] for package in f.read().split("\n\n")], key=lambda x:sum(x))
 print(sum(packages[-1]), sum(map(sum, packages[-3:])))
